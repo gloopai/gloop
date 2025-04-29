bb8e7101928f8ac563f0677a2cad90f2d0d9b8df
eq <eq@d.com>
2025-04-29 18:21:35 +0800

    feat: 增加 component

--- container.go
+++ container.go
@@ -1,1 +1,12 @@
 package gloop
+
+import "github.com/gloogai/gloop/component"
+
+type Container struct {
+	components []component.Component
+}
+
+// NewContainer 创建一个容器
+func NewContainer() *Container {
+	return &Container{}
+}
