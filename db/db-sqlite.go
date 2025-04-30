60d70b10f7ea34e5cbb9da7a65337d58d7df2578
eq <eq@d.com>
2025-04-30 21:16:43 +0800

    feat: 增加 sqlite 数据库操作

---
+++ db/db-sqlite.go
@@ -0,0 +1,57 @@
+package db
+
+import (
+	"database/sql"
+	"log"
+
+	_ "modernc.org/sqlite"
+
+	"github.com/gloopai/gloop/component"
+	"github.com/gloopai/gloop/lib"
+)
+
+type DbSqlite struct {
+	component.Base
+	Path string // 数据库路径
+	Db   *sql.DB
+}
+
+// NewDb 创建一个新的数据库实例
+func NewDbSqlite(dbpath string) *DbSqlite {
+	return &DbSqlite{
+		Path: dbpath,
+	}
+}
+
+func (d *DbSqlite) Name() string {
+	return "db"
+}
+
+// 修改 Init 方法以保存数据库连接，并提供一个方法获取连接
+func (d *DbSqlite) Init() {
+	lib.Log.Info("Initializing SQLite database at path:", d.Path)
+	// 初始化数据库连接
+	db, err := sql.Open("sqlite", d.Path)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// 将数据库连接保存到结构体中
+	d.Db = db
+
+	// _, err = db.Exec("CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY, name TEXT)")
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+	lib.Log.Info("SQLite database initialized successfully")
+}
+
+/*  */
+func (d *DbSqlite) Close() {
+	d.Db.Close()
+}
+
+// 提供一个方法来获取数据库连接
+func (d *DbSqlite) GetConnection() *sql.DB {
+	return d.Db
+}
--- site/proxy.go
+++ site/proxy.go
@@ -1,7 +1,10 @@
 package site
 
+import "github.com/gloopai/gloop/db"
+
 type Proxy struct {
 	Site *Site
+	Db   *db.DbSqlite
 }
 
 func NewProxy(site *Site) *Proxy {
